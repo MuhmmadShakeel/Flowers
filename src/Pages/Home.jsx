23d6cf255f36dd6cd734c501f7d105c86b00944f
Sadiq Hussain <sa7434542@gmail.com>
2025-12-25 12:28:22 +0500

    set path

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import Header from '../omponents/Home/Header'
+import Header from "../components/Home/Header"
+import AboutUs from "../components/Home/AboutUs"
+import WhyChooseUs from "../components/Home/WhyChooseUs"
+import MeetMyTeam from "../components/Home/MeetMyTeam"
+import Faq from "../components/Home/Faq"
+
 function Home() {
   return (
     <div>
